7d5070da4ce859490420a6bb5b0a008cf312876f
Seeratfatima11 <seeratfatima.1596@gmail.com>
2025-04-10 12:58:17 +0500

    header, home, counter and promo section added

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+// On Scroll Function
+function navMenu(){
+    let navBar = document.querySelector('.navbar-sticky');
+    let scrollTopButtion = document.querySelector('#scrollUp');
+
+    window.onscroll = function(){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 120){
+            navBar.classList.add("navbar-sticky-moved-up");
+        }else{
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+
+        //Apply Transition
+        if(scroll >= 250){
+            navBar.classList.add("navbar-sticky-transitioned");
+            scrollTopButtion.classList.add("scrollActive");
+        }else{
+            navBar.classList .remove("navbar-sticky-transitioned");
+            scrollTopButtion.classList.add(scrollActive);
+        }
+        //Sticky On
+        if(scroll >= 500){
+            navBar.classList.add("navbar-sticky-on");
+        }else{
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    }
+}
+
+
+//Counter design
+document.addEventListener("DOMContentLoaded", () =>{
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() =>{
+            current +=increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0,10,2000);
+    counter("count2", 100,20,3000);
+    counter("count3", 0,30,4000);
+    counter("count4", 0,10,2000);
+});
+
+
+//APP-SCREEN
+
+
+//Side Navigation Bar Close While We Click On Navigation Links
